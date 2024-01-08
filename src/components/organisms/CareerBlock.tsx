590b1cf366c5729defab18b51a17b676796bcf7c
Kyrill <kyrill@gobber.ch>
2024-01-08 14:53:03 +0100

    feat: translation things for career block

--- src/components/organisms/CareerBlock.tsx
+++ src/components/organisms/CareerBlock.tsx
@@ -1,15 +1,24 @@
 import { useTranslation } from "react-i18next";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
 import { Experience, Experiences } from "@/lib/constants";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "../ui/accordion";
 
 const getCareerItems = () => {
     return Experiences.map((experience, index) => {
-        return (
-            <CareerItem key={index} experience={experience} />
-        );
+        return <CareerItem key={index} experience={experience} />;
     });
-}
+};
 
 export const CareerBlock = () => {
     const { t } = useTranslation();
@@ -27,17 +36,15 @@ export const CareerBlock = () => {
 };
 
 type CareerItemProps = {
-    experience: Experience
-}
+    experience: Experience;
+};
 
 const TaskItems = ({ tasks }: { tasks: string[] }) => {
     const { t } = useTranslation();
     return tasks.map((task, index) => {
-        return (
-            <p key={index}>- {t(task)}</p>
-        );
-    })
-}
+        return <p key={index}>- {t(task)}</p>;
+    });
+};
 
 const CareerItem = ({ experience }: CareerItemProps) => {
     const { t } = useTranslation();
@@ -51,15 +58,19 @@ const CareerItem = ({ experience }: CareerItemProps) => {
                 </span>
             </div>
             <div>
-                <Accordion type="single" collapsible>
+                <Accordion type="single" collapsible defaultValue="item-1">
                     <AccordionItem value="item-1">
-                        <AccordionTrigger>{t('career.experienceDesc')}</AccordionTrigger>
+                        <AccordionTrigger>
+                            {t("career.experienceDesc")}
+                        </AccordionTrigger>
                         <AccordionContent>
                             {t(experience.desc)}
                         </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="item-2">
-                        <AccordionTrigger>{t('career.skillsAndTechstack')}</AccordionTrigger>
+                        <AccordionTrigger>
+                            {t("career.skillsAndTechstack")}
+                        </AccordionTrigger>
                         <AccordionContent>
                             <TaskItems tasks={experience.tasks} />
                         </AccordionContent>
@@ -68,4 +79,4 @@ const CareerItem = ({ experience }: CareerItemProps) => {
             </div>
         </div>
     );
-}
+};
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -71,14 +71,56 @@ export const Experiences: Experience[] = [
         ],
     },
     {
-        title: "experiences.nu.title",
-        company: "experiences.nu.companyName",
-        time: "experiences.nu.time",
-        desc: "experiences.nu.desc",
+        title: "experiences.cleoAG.title",
+        company: "experiences.cleoAG.companyName",
+        time: "experiences.cleoAG.time",
+        desc: "experiences.cleoAG.desc",
         tasks: [
-            "experiences.nu.task1",
-            "experiences.nu.task2",
-            "experiences.nu.task3",
+            "experiences.cleoAG.task1",
+            "experiences.cleoAG.task2",
+            "experiences.cleoAG.task3",
+        ],
+    },
+    {
+        title: "experiences.sgkb.title",
+        company: "experiences.sgkb.companyName",
+        time: "experiences.sgkb.time",
+        desc: "experiences.sgkb.desc",
+        tasks: [
+            "experiences.sgkb.task1",
+            "experiences.sgkb.task2",
+            "experiences.sgkb.task3",
+        ],
+    },
+    {
+        title: "experiences.aba.title",
+        company: "experiences.aba.companyName",
+        time: "experiences.aba.time",
+        desc: "experiences.aba.desc",
+        tasks: [
+            "experiences.aba.task1",
+            "experiences.aba.task2",
+            "experiences.aba.task3",
+        ],
+    },
+    {
+        title: "experiences.mil.title",
+        company: "experiences.mil.companyName",
+        time: "experiences.mil.time",
+        desc: "experiences.mil.desc",
+        tasks: [
+            "experiences.mil.task",
+        ],
+    },
+    {
+        title: "experiences.gzi.title",
+        company: "experiences.gzi.companyName",
+        time: "experiences.gzi.time",
+        desc: "experiences.gzi.desc",
+        tasks: [
+            "experiences.gzi.task1",
+            "experiences.gzi.task2",
+            "experiences.gzi.task3",
         ],
     },
 ];
