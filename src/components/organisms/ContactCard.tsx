21c6e6c7fbf81c2fc13989d423fff21157b23ae7
Kyrill <Kyrill.gobber@cleoag.ch>
2024-01-09 02:06:09 +0100

    feat: contact card should be fine for now

--- src/components/organisms/ContactCard.tsx
+++ src/components/organisms/ContactCard.tsx
@@ -1,19 +1,30 @@
 import { useTranslation } from "react-i18next";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
 
 export const ContactCard = () => {
     const { t } = useTranslation();
     return (
-        <Card>
+        <Card className="self-center">
             <CardHeader>
-                <CardTitle>{t('contact.title')}</CardTitle>
-                <CardDescription>{t('contact.desc')}</CardDescription>
+                <CardTitle>{t("contact.title")}</CardTitle>
+                <CardDescription>{t("contact.desc")}</CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>{t('contact.name')}</p>
-                <p>{t('contact.email')}</p>
+            <CardContent className="flex flex-col justify-center items-center">
+                <a
+                    target="_blank"
+                    href={"mailto:info@gobber.ch"}
+                    rel="noopener noreferrer"
+                >
+                    <Button variant="secondary">{t("contact.email")}</Button>
+                </a>
             </CardContent>
         </Card>
     );
-}
-
+};
